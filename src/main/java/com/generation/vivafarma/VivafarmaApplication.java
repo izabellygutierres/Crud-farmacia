1a66d1cd79ed04364b7bdc580b31a10d561bf55e
Izabelly Gutierres Silva <izabellygutierressilva@email.com>
2025-01-27 10:30:08 -0300

    Criação de conexão com repositorio remoto

---
+++ src/main/java/com/generation/vivafarma/VivafarmaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.vivafarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivafarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivafarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/vivafarma/VivafarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.vivafarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivafarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
